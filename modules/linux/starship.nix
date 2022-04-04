ca951086d734cb03d0cc828afd40f8d61bea0f2a
David Alexander <opensource@thelonelyghost.com>
2022-04-03 23:16:42 -0400

    Removes cruft after refactor

--- modules/linux/starship.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, isLinux, ... }:
-# vim: ts=2 sts=2 sw=2 et
-
-assert isLinux;
-
-{
-  programs.starship.settings.battery.format = "[Batt:$percentage]($style)";
-  programs.starship.settings.nodejs.symbol = "JS ";
-}
--- modules/ssh.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-# vim: ts=2 sts=2 sw=2 et
-
-{
-  home.packages = [
-    pkgs.openssh
-  ];
-}
