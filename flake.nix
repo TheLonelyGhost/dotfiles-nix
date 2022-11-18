1ae9732241527c6fb3ed4dcf822895b6bcdb354e
David Alexander <opensource@thelonelyghost.com>
2022-11-18 18:23:58 -0500

    Removes buffalo tooling. Not very nix-friendly

--- flake.nix
+++ flake.nix
@@ -19,20 +19,20 @@
     workstation-deps.inputs.flake-utils.follows = "flake-utils";
     workstation-deps.inputs.flake-compat.follows = "flake-compat";
     workstation-deps.inputs.overlays.follows = "overlays";
-    golang-webdev.url = "github:thelonelyghost/golang-webdev-nix";
-    golang-webdev.inputs.flake-utils.follows = "flake-utils";
-    golang-webdev.inputs.flake-compat.follows = "flake-compat";
-    golang-webdev.inputs.overlays.follows = "overlays";
+    # golang-webdev.url = "github:thelonelyghost/golang-webdev-nix";
+    # golang-webdev.inputs.flake-utils.follows = "flake-utils";
+    # golang-webdev.inputs.flake-compat.follows = "flake-compat";
+    # golang-webdev.inputs.overlays.follows = "overlays";
 
     zsh-plugin-syntax-highlight.url = "github:zdharma-continuum/fast-syntax-highlighting";
     zsh-plugin-syntax-highlight.flake = false;
   };
 
-  outputs = { self, nixpkgs, home-manager, flake-compat, flake-utils, overlays, neovim, workstation-deps, golang-webdev, zsh-plugin-syntax-highlight }:
+  outputs = { self, nixpkgs, home-manager, flake-compat, flake-utils, overlays, neovim, workstation-deps, zsh-plugin-syntax-highlight }:
     let
       baseConfig = import ./baseConfig.nix {
         /* Flakes that need resolving per system */
-        inherit nixpkgs home-manager flake-compat flake-utils neovim workstation-deps golang-webdev overlays;
+        inherit nixpkgs home-manager flake-compat flake-utils neovim workstation-deps overlays;
       } {
         /* Non-Flake flakes */
         inherit zsh-plugin-syntax-highlight;
--- modules/golang.nix
+++ modules/golang.nix
@@ -1,15 +1,8 @@
-{ pkgs, golang-webdev, ... }:
+{ pkgs, ... }:
 # vim: ts=2 sts=2 sw=2 et
 
 {
   home.packages = [
     pkgs.go
-    golang-webdev.buffalo
-    golang-webdev.buffalo-plugin-heroku
   ];
-
-  home.file.".buffalo.yml".text = ''
-  ---
-  db-type: sqlite3
-  '';
 }
